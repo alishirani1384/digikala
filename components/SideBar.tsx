25da1a6e173e888e63f409220fbd23eb17795c2d
Ali Shirani <alishiranii1384@gmail.com>
2023-03-19 11:48:40 +0330

    added some animations to sidbar

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -7,21 +7,37 @@ import Link from "next/link";
 import React from "react";
 import Logo from "../app/assets/images/logo/digi.svg";
 import { shallow } from "zustand/shallow";
+import { motion,AnimatePresence } from "framer-motion";
 
 function SideBar() {
   const { open, closeIt } = useSidebarStore(
     (state) => ({ open: state.open, closeIt: state.closeIt }),
     shallow
   );
+  const variants1 = {
+    open: { opacity: 1},
+    closed: { opacity: 0, display:"none" }
+  }
+  const variants3 = {
+    open: { opacity: .5},
+    closed: { opacity: 0, display:"none" }
+  }
+  const variants2 = {
+    open: {
+      x: 0,
+      transition: { duration: 0.03 },
+    },
+    closed: { x: "100%" },
+  };
   return (
-    <div
-      className={`fixed top-0 left-0 right-0 bottom-0 lg:hidden ${
-        open ? "block" : "hidden"
-      }`}>
-      <div
-        className={`bg-white w-2/3 md:w-1/2 transition-transform delay-150 absolute top-0 bottom-0 right-0 ${
-          open ? "translate-x-0" : "translate-x-[1000px]"
-        }`}>
+    <motion.div
+      animate={open ? "open" : "closed"}
+      variants={variants1}
+      className={`fixed top-0 left-0 right-0 bottom-0 lg:hidden`}>
+      <motion.div
+        animate={open ? "open" : "closed"}
+        variants={variants2}
+        className={`bg-white z-50 w-2/3 md:w-1/2 transition-transform delay-150 absolute top-0 bottom-0 right-0`}>
         <div className="p-3 flex flex-col gap-5">
           <Link href={"/"}>
             <Image width={100} src={Logo} alt="logo" />
@@ -39,13 +55,14 @@ function SideBar() {
             })}
           </div>
         </div>
-      </div>
-      <div
+      </motion.div>
+
+      <motion.div
+        animate={open ? "open" : "closed"}
+        variants={variants3}
         onClick={closeIt}
-        className={`w-1/3 md:w-1/2 absolute top-0 left-0 bottom-0 bg-black  opacity-50 ${
-          open ? "block" : "hidden"
-        }`}></div>
-    </div>
+        className={`w-full md:w-1/2 absolute top-0 left-0 bottom-0 bg-black`}></motion.div>
+    </motion.div>
   );
 }
 
